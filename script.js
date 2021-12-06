a04170268901674f356651396b0a36d1492627a8
PauloC88 <93497223+PauloC88@users.noreply.github.com>
2021-12-07 00:42:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let messageBox1 = document.querySelector("div1");
+let messageBox2 = document.querySelector("div2");
+
+function validateFormat(input) {
+  return /^[a-z]{2,35}$/.test(input);
+}
+
+function addWord() {
+  // remove all previous messages from the page;
+  messageBox1.innerHTML = "";
+  messageBox2.innerHTML = "";
+  let word = document.getElementById("word").value;
+  word = word.toLowerCase();
+  let translation = document.getElementById("translation").value;
+  translation = translation.toLowerCase();
+  let wordList = document.getElementById("wordList");
+  if (validateFormat(word) && validateFormat(translation)) {
+    wordList.innerHTML += "<li class=\"word-item\">" + word + "</li><li class=\"word-item\">" + translation + "</li>";
+  } else {
+    messageBox1.innerHTML = "Introduceți doar litere! Minim 2, maxim 35.";
+  }
+  return false;
+}
+    
+function searchWord() {
+  // remove all previous messages from the page;
+  messageBox1.innerHTML = "";
+  messageBox2.innerHTML = "";
+  let searchedWord = document.getElementById("searchbox").value
+  searchedWord = searchedWord.toLowerCase();
+  let words = document.getElementsByClassName("word-item"); 
+  let wordExists = 0; 
+  for (i = 0; i < words.length; i += 2) { 
+    if (words[i].innerText === searchedWord) {
+      wordExists = 1;
+    }
+  }
+  if (wordExists === 0) {
+    messageBox2.innerHTML = "<strong>&nbsp Cuvântul căutat nu există în dicționar!</strong><br>";
+  } else {
+    messageBox2.innerHTML = "<strong>&nbsp Cuvântul căutat există în dicționar!</strong><br>";
+  }
+  return false;
+}
